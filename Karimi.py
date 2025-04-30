08f07cf27e66f091149146e25bd4fbdb2c6c1dc8
avilabeto50 <113048974+avilabeto50@users.noreply.github.com>
2025-04-29 22:42:30 -0500

    2nd algorithm done

---
+++ Karimi.py
@@ -0,0 +1,252 @@
+#DNA Encryption
+
+
+##KEY GENERATION
+#convert bits into DNA
+def dna_encode(binary):
+    
+    #convert to DNA
+    dna = ''
+
+    for i in range(0, len(binary), 2):
+        if binary[i] == '0' and binary[i+1] == '0':
+            dna += 'A'
+        elif binary[i] == '0' and binary[i+1] == '1':
+            dna += 'G'
+        elif binary[i] == '1' and binary[i+1] == '0':
+            dna += 'T'
+        elif binary[i] == '1' and binary[i+1] == '1':
+            dna += 'C'
+    
+
+    if len(dna) < 60:
+        for i in range(0,len(dna)):
+            dna += dna[i]
+            if len(dna) >= 60:
+                break
+    
+    #last element of dna
+    last = dna[-1]
+
+    #if the length of dna is not a multiple of 3, replicate the last element until it is
+    while len(dna)%3 != 0:
+        dna += last
+
+        
+    return dna
+    
+#combines two DNA strands
+def Anneal(key):
+
+    new_key = key
+    for char in key:
+        if char == 'A':
+            new_key += 'T'
+        elif char == 'T':
+            new_key += 'A'
+        elif char == 'C':
+            new_key += 'G'
+        elif char == 'G':
+            new_key += 'C'
+    
+    return new_key
+
+##transcription of DNA to mRNA
+def transcription(dna):
+
+    mRNA = ''
+    for char in dna:
+        if char == 'A':
+            mRNA += 'A'
+        elif char == 'T':
+            mRNA += 'U'
+        elif char == 'C':
+            mRNA += 'C'
+        elif char == 'G':
+            mRNA += 'G'
+    
+    return mRNA
+
+##mutation of DNA to mRNA
+def mutation(dna):
+
+    stop_codon = ['UAA', 'UAG', 'UGA']
+
+    mutation_map = [
+    ('CAG', 'UAG'),
+    ('CGA', 'UGA'),
+    ('UGG', 'UGA'),
+    ('GAU', 'GUU'),
+    ('GAG', 'GUG'),
+    ('GCC', 'ACC')
+    ]
+
+    codons = [dna[i:i+3] for i in range(0, len(dna), 3)]
+
+    for i in range(len(codons)):
+        for rule in mutation_map:
+            if codons[i] == rule[0]:
+                codons[i] = rule[1]
+                break
+    
+    dna = '' 
+    for codon in codons:
+        dna += codon
+
+
+    keys = [[]]
+    stops = 0
+
+    #check for stop codon
+    for i in range(0, len(dna), 3):
+        codon = dna[i:i+3]
+        keys[0] += codon
+        if stops > 0:
+            for i in range(1, stops+1):
+                keys[i] += codon
+        if codon in stop_codon:
+            stops += 1
+            keys.append([])
+            continue
+    
+    #turn the keys into a string
+    for i in range(len(keys)):
+        keys[i] = ''.join(keys[i])
+
+    return keys
+    
+def generate_keys(key):
+    
+    #encode to DNA
+    dna = dna_encode(key)
+    
+    #combine the DNA strands
+    new_key = Anneal(dna)
+
+    new_key = transcription(new_key)
+
+    keys = mutation(new_key)
+
+    for i in range(len(keys)):
+        keys[i] = dna_decode(keys[i])
+
+    #convert to binary
+    for i in range(len(keys)):
+        keys[i] = binary_blocks(keys[i])
+    
+    return keys
+
+##ENCRYPTION
+def dna_decode(dna):
+    binary = ''
+    for char in dna:
+        if char == 'A':
+            binary += '00'
+        elif char == 'G':
+            binary += '01'
+        elif char == 'T' or char == 'U':
+            binary += '10'
+        elif char == 'C':
+            binary += '11'
+    
+    return binary
+
+def binary_blocks(binary):
+    
+    #split the binary string into blocks of 8 bits
+    blocks = []
+    for i in range(0, len(binary), 8):
+        block = binary[i:i+8]
+        blocks.append(block)
+    
+    #if the last block is not 8 bits, pad it with 0s
+    if len(blocks[-1]) < 8:
+        blocks[-1] = blocks[-1].ljust(8, '0')
+    
+    return blocks
+
+def encryption(blocks, message):
+
+    ciphertext = []
+
+    for plain in message:
+        m = plain
+        for i, key_block in enumerate(blocks, start=1):
+            m = (m << i) 
+            for k in key_block:
+                m ^= k
+        ciphertext.append(m)
+     
+    return ciphertext
+
+def encrypt_message(message, keys):
+    #turn message into binary using ascii
+    message = ''.join(format(ord(i), '08b') for i in message)
+
+    #break message into blocks of 8 bits
+    message_blocks = []
+    #pad the message with 0s if it is not a multiple of 8
+    if len(message)%8 != 0:
+        message = message.ljust(len(message)+8-len(message)%8, '0')
+    for i in range(0, len(message), 8):
+        block = message[i:i+8]
+        message_blocks.append(block)
+
+    #turn everything into binary
+    for i in range(0,len(message_blocks)):
+        message_blocks[i] = int(message_blocks[i],2)
+
+    for i in range(0,len(keys)):
+        for j in range(0,len(keys[i])):
+            keys[i][j] = int(keys[i][j],2)
+
+
+    ciphertext = encryption(keys, message_blocks)
+
+    #turn the ciphertext into a string
+    ciphertext_string = ''.join(format(i, '08b') for i in ciphertext)
+    
+    return [ciphertext, ciphertext_string]
+
+
+#DECRYPTION
+def decryption(blocks, ciphertext):
+    decrypted_message = []
+    for cm in ciphertext:
+        m = cm
+        for i, key_block in reversed(list(enumerate(blocks, start=1))):
+            for k in reversed(key_block):
+                m ^= k
+            m = (m >> i) 
+        decrypted_message.append(m)
+    return decrypted_message
+
+def decrypt_message(ciphertext, keys):
+    ciphertext_binary = ''
+    for i in range(0,len(ciphertext)):
+        ciphertext_binary += format(ciphertext[i], '08b')
+
+    decrypted_message = decryption(keys, ciphertext)
+    decrypted_message = ''.join(format(i, '08b') for i in decrypted_message)
+    decrypted_message = ''.join(chr(int(decrypted_message[i:i+8], 2)) for i in range(0, len(decrypted_message), 8))
+
+    return decrypted_message
+
+def main():
+   
+    password = input("Enter your password: ")
+    keys = generate_keys(password)
+
+    message = input("Enter the message: ")
+
+
+    ciphertext = encrypt_message(message, keys)
+
+    print(f"Ciphertext: {ciphertext[1]}")
+
+    decrypted_message = decrypt_message(ciphertext[0], keys)
+    print(f"Decrypted message: {decrypted_message}")
+    
+    
+
+main()
--- dna_cryptography.py
+++ dna_cryptography.py
@@ -1,4 +1,4 @@
-import rahti_improved, os
+import rahti_improved, os, Karimi
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -8,13 +8,18 @@ def main():
     while True:
         print('Select an algorithm using the respective number.')
         print('(1) Rahti Algorithm')
-        print('(2) Exit program.')
+        print('(2) Karimi Algorithm')
+        print('(3) Exit program.')
         number = str(input())
 
         match number:
             case '1':
                 rahti_demonstration()
+                break
             case '2':
+                karimi_demonstration()
+                break
+            case '3':
                 print('We appreciate your time here!')
                 break
             case _:
@@ -73,6 +78,60 @@ def rahti_demonstration():
             case _:
                 print('Input not recognized!')
 
+
+def karimi_demonstration():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('You have selected the Karimi Algorithm.')
+    key = None
+    plaintext = None
+    ciphertext = None
+
+    while True:
+        print('Select an option using the respective number.')
+        print('(1) Generate keys.')
+        print('(2) Generate ciphertext.')
+        print('(3) Generate plaintext.')
+        print('(4) Exit demonstration.')
+        number = str(input())
+
+        match number:
+            case '1':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                password = input('Input a password: ')
+                key = Karimi.generate_keys(password)
+                print('Your newly generated keys are: {}.'.format((key)))
+
+                if not ciphertext == None:
+                    print('Alert! Reseting current ciphertext! Please generate a new ciphertext.')
+                    ciphertext = None
+            case '2':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                if key == None:
+                    print('You need to generate a key first!')
+                else:
+                    while True:
+                        plaintext = input('Input a message text: ')
+                        try:
+                            ciphertext = Karimi.encrypt_message(plaintext,key)
+                            print('Your newly encrypted ciphertext is: {}.'.format((ciphertext[1])))
+                            break
+                        except:
+                            print('Error! Plaintext exceeds 64 characters!')
+            case '3':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                if key == None:
+                    print('You need to generate a key first!')
+                elif ciphertext == None:
+                    print('You need to generate a ciphertext first!')
+                else:
+                    plaintext_1 = Karimi.decrypt_message(ciphertext[0],key)
+                    print('Your newly decrypted plaintext is: {}.'.format((plaintext_1)))
+            case '4':
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Returning to main menu!')
+                break
+            case _:
+                print('Input not recognized!')
 
 if __name__ == "__main__":
     main()
